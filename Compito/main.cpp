355a7902992da8774060c16f224ba2d784dd3aba
Luca Masini <luca.masini@gmail.com>
2021-06-19 15:38:48 +0200

    Initial Commit

---
+++ Compito/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Compito
+//
+//  Created by Luca Masini on 19/06/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
